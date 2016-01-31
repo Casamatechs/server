7551f334719b847b8c0756573a161b5c2ac8d02a
Carlos Sanchez <casamatechs@gmail.com>
2016-01-31 15:53:11 +0100

    Added a function that adds universities to the database

--- db_operations.py
+++ db_operations.py
@@ -7,5 +7,10 @@ def conexion(raiz_db):
     db = Database('sqlite', raiz_db, create_db=False)
     print "Se ha realizado la conexion a la base de datos"
     return db
-
+@db_session
+def insert_data_uni(id, nombre, raiz_db):
+    db = conexion(raiz_db)
+    db.insert("Universidad", idUni= id, NombreUni=nombre)
+    db.commit()
+    
 
